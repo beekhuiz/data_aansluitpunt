628140362a0f01568eceb372384d600ba4233eb3
beekhuiz <johan.beekhuizen@deltares.nl>
2016-02-25 14:37:57 +0100

    Added getlocations, getparameters, getavg

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask, jsonify, request, render_template, session, redirect, url_for, flash, send_from_directory
 from pymongo import MongoClient
 import requests
-import gis_functions
+#import gis_functions       # NOT NEEDED (YET)
+import json
+from operator import itemgetter  # used for sorting dictionary lists of unique locations, parameters and sources alphabetically
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gukfdshkjdsoipee'
@@ -13,6 +15,99 @@ app.config['DEVELOP'] = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/locations/')
+def getLocations():
+
+    client = MongoClient()
+    db = client.EI_Toets
+    collection = db["EIdata"]
+
+    searchDict = {} # potential for selecting a subset
+    mongocursor = collection.find(searchDict)
+
+    timeseries = []
+
+    for record in mongocursor:
+        del record['_id'] # remove mongoID, that should not be part of the output (and is not JSON Serializable)
+        timeseries.append(record)
+
+    uniqLocIDSeries = list({v['properties']['locID']: v for v in timeseries}.values())
+
+    uniqLocList = []
+    for uniqLocIDSerie in uniqLocIDSeries:
+        uniqLoc = {}
+        uniqLoc['type'] = 'Feature'
+        uniqLoc['properties'] = {}
+        uniqLoc['properties']['source'] = uniqLocIDSerie['properties']['source']
+        uniqLoc['properties']['locID'] = uniqLocIDSerie['properties']['locID']
+        uniqLoc['properties']['locName'] = uniqLocIDSerie['properties']['locName']
+        uniqLoc['geometry'] = uniqLocIDSerie['geometry']
+        uniqLocList.append(uniqLoc)
+
+    uniqLocList.sort(key=lambda e: e['properties']['locName'])  # sort alphabetically on locName
+
+    uniqLocationsDict = {}
+    uniqLocationsDict['type'] = "FeatureCollection"
+    uniqLocationsDict['features'] = uniqLocList
+
+    return json.dumps(uniqLocationsDict)
+
+
+@app.route('/parameters/')
+def getParameters():
+
+    client = MongoClient()
+    db = client.EI_Toets
+    collection = db["EIdata"]
+
+    searchDict = {} # potential for selecting a subset
+    mongocursor = collection.find(searchDict)
+
+    timeseries = []
+
+    for record in mongocursor:
+
+        del record['_id'] # remove mongoID, that should not be part of the output (and is not JSON Serializable)
+        timeseries.append(record)
+
+    uniqParIDSeries = list({v['properties']['parID']: v for v in timeseries}.values())
+    uniqParList = []
+    for uniqParIDSerie in uniqParIDSeries:
+        uniqParList.append({'parID': uniqParIDSerie['properties']['parID'],
+                            'parName': uniqParIDSerie['properties']['parName'],
+                            'parDesc': uniqParIDSerie['properties']['parDescription']})
+    uniqParList = sorted(uniqParList, key=itemgetter('parName'))  # sort alphabetically
+
+    return json.dumps(uniqParList)
+
+
+@app.route('/avg')
+def getAverage():
+
+    parID = request.args['parID']
+    locID = request.args['locID']
+
+    client = MongoClient()
+    db = client.EI_Toets
+    collection = db["EIdata"]
+
+    searchList = []
+    searchDict = {} # potential for selecting a subset
+
+    searchList.append({"properties.parID": parID})
+    searchList.append({"properties.locID": locID})
+    searchDict["$and"] = searchList
+
+    mongocursor = collection.find(searchDict)
+
+    timeseries = []
+    for record in mongocursor:
+        del record['_id'] # remove mongoID, that should not be part of the output (and is not JSON Serializable)
+        timeseries.append(record)
+
+    return json.dumps(timeseries)
 
 
 
